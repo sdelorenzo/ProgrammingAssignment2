16f6585aa20135ad1bddeadd055536dbfc9510db
Stephen DeLorenzo <stephen.delorenzo@gmail.com>
2017-04-28 21:41:33 -0700

    update for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## two functions working together to save system resources in the event
+## that an inverse of the same square matrix must be used repeatedly
 
-## Write a short comment describing this function
+##make a blank matrix to store an inverse, set variables/functions for use in
+##the next function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                inv <- NULL
+                set <- function(y){
+                    x <<- y
+                    inv <<- NULL
+                }
+                get <- function() x
+                setinverse <- function(solve) inv <<- solve
+                getinverse <- function() inv
+                list(set = set, get = get,
+                     setinverse = setinverse,
+                     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## calculate the inverse of the given matrix if it hasnt been calculated already
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+          inv <- x$getinverse()
+          if(!is.null(inv)){
+              message("getting cached data")
+            return(inv)
+          }
+          data <- x$get()
+          inv <- solve(data, ...)
+          x$setinverse(inv)
+          inv
 }
